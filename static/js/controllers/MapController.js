5aa8a84ae86188a1b719e644f6dbb8d2069d653a
Robin Désir <desir@ece.fr>
2017-02-16 00:39:12 -0500

    Fix markers: resolve only 100

--- static/js/controllers/MapController.js
+++ static/js/controllers/MapController.js
@@ -5,44 +5,44 @@ angular.module('myApp')
   console.log('Map Controller')
   get_profiles = ProfilesService.getProfiles();
   $scope.profiles = get_profiles
+
+
   $scope.markers = []
 
   idKey = 0
+
 
   $scope.map = {
     center: {
       latitude: 48.8628,
       longitude: 2.3292
     },
-    zoom: 3,
-    bounds: {
-      northeast: {
-        latitude: 45.1451,
-        longitude: -80.6680
-      },
-      southwest: {
-        latitude: 30.000,
-        longitude: -120.6680
-      }
-    }
-  };
-  $scope.options = {
-    scrollwheel: false
+    zoom: 3
   };
 
+  $scope.markers = [];
+
+    $scope.clickMarker = function(marker) {
+      alert(marker.ip); 
+    };
+
+
   $scope.createMarkers = function(){
-    for (var i = 0; i < $scope.profiles.length-1; i++) {
-        var ret = {
-            id: i+1,
+    for (var i = 1; i <= 100; i++) {
+        var m = {
+            id: i,
             coord:{
               latitude: $scope.profiles[i].latitude,
               longitude: $scope.profiles[i].longitude  
-            }
+            },
+            icon: "https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi.png",
+            ip:$scope.profiles[i].ip
         }
 
-        $scope.markers.push(ret);
+        $scope.markers.push(m);
     }  
   }
+
     
   $scope.createMarkers();
 })
